10757f29b43334fef9f6e8482a4fd87c0f3980f0
Anthony Vallee <tony.vallee@hotmail.com>
2016-09-08 16:32:43 -0400

    Fixes tests/lib destination path, reorders actions

--- generators/app/index.js
+++ generators/app/index.js
@@ -52,20 +52,19 @@ module.exports = yeoman.Base.extend({
   writing: function() {
 
     const actions = [
-      { templatePath: 'app/**/*', destinationPath: `${ this.props.appName }`, template: true },
-      { templatePath: 'app/static/styles/app.css', destinationPath: `${ this.props.appName }/static/styles/${ this.props.appName }.css`, template: false },
-      { templatePath: 'app/templates/styles/app.jinja2', destinationPath: `${ this.props.appName }/templates/styles/${ this.props.appName }.jinja2`, template: false },
+
+      { templatePath: 'app/**', destinationPath: 'app', template: true },
       { templatePath: 'config/*', destinationPath: 'config', template: true },
-      { templatePath: 'instance/*', destinationPath: 'instance', template: true },
-      { templatePath: 'lib/*', destinationPath: 'lib', template: true },
-      { templatePath: 'test/app/*', destinationPath: `tests/${ this.props.appName }`, template: false },
-      { templatePath: 'tests/lib/*', destinationPath: 'tests/lib', template: false },
       { templatePath: '.gitignore', destinationPath: '.gitignore', template: true },
-      { templatePath: 'LICENSE', destinationPath: 'LICENSE', template: false },
       { templatePath: 'manage.py', destinationPath: 'manage.py', template: true },
       { templatePath: 'readmefile.md', destinationPath: 'README.md', template: true },
+
+      { templatePath: 'instance/*', destinationPath: 'instance', template: false },
+      { templatePath: 'tests/**', destinationPath: 'tests', template: false },
+      { templatePath: 'LICENSE', destinationPath: 'LICENSE', template: false },
       { templatePath: 'requirements.txt', destinationPath: 'requirements.txt', template: false },
       { templatePath: 'setup.py', destinationPath: 'setup.py', template: true },
+
     ];
 
     actions.forEach((action) => {
